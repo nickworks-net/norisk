969e931702b016e6995fed1ca528895714ec2655
にっく <ccoo.jp@gmail.com>
2018-09-15 19:38:52 +0900

    norisk

---
+++ norisk.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import time
+from datetime import datetime
+
+
+while True:
+    # No pain, No gain.
+    # これはより多くのトレーダーさんにオススメされて輝くコンテンツです。
+    # シェアお願いします。
+    print( datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ' リスクがあるためスルーします' )
+    time.sleep(5)
